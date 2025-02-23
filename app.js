35033441811c40d638ece41eef481833018d849a
shaikhhamza11 <shaikh.hamza1199@gmail.com>
2025-02-23 16:35:18 +0530

    FEAT : Added middlewares

--- app.js
+++ app.js
@@ -7,12 +7,20 @@ import {
   subscriptionRouter,
 } from './routes/index.routes.js';
 import connectToDatabase from './database/mongodb.js';
+import errorMiddleware from './middleware/error.middleware.js';
+import cookieParser from 'cookie-parser';
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/user', userRouter);
 app.use('/api/v1/subcription', subscriptionRouter);
+
+app.use(errorMiddleware);
 
 app.get('/', (req, res) => {
   res.send('Welcome to Subcription Tracker API');
