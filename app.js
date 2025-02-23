e95b9e10e9dc2434bc770a4e0c21e60b14b97ec0
shaikhhamza11 <shaikh.hamza1199@gmail.com>
2025-02-23 20:54:15 +0530

    FEAT: Rate limiting configured using Arcjet

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 import express from 'express';
 import { PORT } from './config/env.js';
 import chalk from 'chalk';
-import { authRouter, userRouter, subscriptionRouter } from './routes/index.routes.js';
+import {
+  authRouter,
+  userRouter,
+  subscriptionRouter,
+} from './routes/index.routes.js';
 import connectToDatabase from './database/mongodb.js';
 import errorMiddleware from './middleware/error.middleware.js';
 import cookieParser from 'cookie-parser';
+import arcjetMiddleware from './middleware/arcjet.middleware.js';
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(arcjetMiddleware);
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
--- middleware/error.middleware.js
+++ middleware/error.middleware.js
@@ -20,7 +20,7 @@ const errorMiddleware = (err, req, res, next) => {
 
     // mongoose validate error
     if (err.name === 'ValidationError') {
-      const message = Object.values(err.errors).map((val) => val.message);
+      const message = Object.values(err.errors).map(val => val.message);
       error = new Error(message.join(', '));
       error.statusCode = 400;
     }
